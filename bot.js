3f8f47ee75fd14b07838967ff22b085b9a8914a6
SonicMuseum <45864859+SonicMuseum@users.noreply.github.com>
2019-02-04 18:45:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,14 +6,14 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    if (message.content === 'help') {
+    	message.channel.send('Do dream.sources for more info.');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
+    if (message.content === 'dream.sources') {
+    	message.reply('A portal to Dreamcast websites https://dreampipe.net');
   	}
 });
 
